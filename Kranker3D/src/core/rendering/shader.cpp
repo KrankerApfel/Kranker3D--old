4513a8c5991723fae838de9246695626b1702615
Tom <t>
2021-05-04 23:19:50 +0200

    add shader class

---
+++ Kranker3D/src/core/rendering/shader.cpp
@@ -0,0 +1,97 @@
+#include "../../../stdafx.h"
+#include "../io/file.h"
+#include "shader.h"
+
+
+GLuint Kranker3D::Shader::compile(std::string shader, GLenum type)
+{
+	GLuint id = glCreateShader(type);
+	const char* shaderCode = shader.c_str();
+
+	glShaderSource(id, 1, &shaderCode, NULL);
+	glCompileShader(id);
+
+	this->compileStatus(id);
+
+	return id;
+}
+
+void Kranker3D::Shader::linkProgramme(GLuint vertexShaderId, GLuint fragmentShaderId)
+{
+	programId = glCreateProgram();
+	if (programId == 0 || programId == GL_INVALID_OPERATION)
+		spdlog::warn("SHADER","Not able to create the shader object properly.\n Program ID :  {}", programId);
+	vertId = vertexShaderId;
+	fragId = fragmentShaderId;
+	glAttachShader(programId, vertId);
+	glAttachShader(programId, fragId);
+	glLinkProgram(programId);
+
+	this->linkStatus();
+
+}
+
+void Kranker3D::Shader::compileStatus(GLuint shaderId)
+{
+	int success;
+	char infoLog[512];
+	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shaderId, 512, NULL, infoLog);
+		spdlog::warn("SHADER"," Shader {} did not compile ! \n===\n%s\n===\n {}", shaderId, infoLog);
+	}
+	else
+	{
+		spdlog::info("SHADER","Shader {} loaded and compiled proprely\n", shaderId);
+	}
+}
+
+void Kranker3D::Shader::linkStatus()
+{
+	int success;
+	GLint linkStatus;
+	char infoLog[512];
+	glGetProgramiv(programId, GL_LINK_STATUS, &linkStatus);
+	if (!linkStatus)
+	{
+		glGetProgramInfoLog(programId, 512, NULL, infoLog);
+		spdlog::error("SHADER", " Program linking failed : \n===\n%s\n===\n {}", infoLog);
+	}
+	else {
+		spdlog::info("SHADER", " Program {} link properly\n", programId);
+		glGetProgramiv(programId, GL_COMPILE_STATUS, &success);
+		if (!success)
+		{
+			glGetProgramInfoLog(programId, 512, NULL, infoLog);
+			spdlog::error("SHADER"," Program did not compile : \n===\n%s\n===\n {}", infoLog);
+		}
+		else
+		{
+			spdlog::info("SHADER", " Program {} loaded and compiled proprely.\n", programId);
+		}
+	}
+
+}
+
+Kranker3D::Shader::Shader(std::string vertex_path, std::string fragment_path)
+{
+	programId = 0;
+	GLuint v_id = this->compile(File::read(vertex_path), GL_VERTEX_SHADER);
+	GLuint f_id = this->compile(File::read(fragment_path), GL_FRAGMENT_SHADER);
+	this->linkProgramme(v_id, f_id);
+}
+
+void Kranker3D::Shader::use()
+{
+	glUseProgram(programId);
+}
+
+void Kranker3D::Shader::terminate()
+{
+	glDetachShader(programId, vertId);
+	glDetachShader(programId, fragId);
+	glDeleteShader(vertId);
+	glDeleteShader(fragId);
+	glDeleteProgram(programId);
+}
---
+++ Kranker3D/src/core/rendering/shader.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../../../stdafx.h"
+#include "../../glm/glm/mat4x4.hpp"
+
+namespace Kranker3D {
+
+	class Shader {
+
+	private:
+		GLuint programId, vertId, fragId;
+
+		GLuint compile(std::string shader, GLenum type);
+		void linkProgramme(GLuint vertexShaderId, GLuint fragmentShaderId);
+		void compileStatus(GLuint shaderId);
+		void linkStatus();
+
+	public:
+		Shader(std::string vertex_path, std::string fragment_path);
+		Shader()=default;
+		void use();
+		GLuint getProgramId() { return programId; }
+		void terminate();
+
+		void setColor(const char* key, GLfloat x, GLfloat y, GLfloat z) { glUniform3f(glGetUniformLocation(programId, key), x, y, z); }
+		void setFloat(const char* key, GLfloat value) { glUniform1f(glGetUniformLocation(programId, key), value); }
+		void setMat4(const char* name, glm::mat4 value) { glUniformMatrix4fv(glGetUniformLocation(programId, name), 1, GL_FALSE, &value[0][0]); }
+
+	};
+}
